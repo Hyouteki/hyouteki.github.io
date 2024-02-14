86f7acdec91d924dbde32085c92d7041e959f309
Lakshay <lakshay21060@iiitd.ac.in>
2024-02-15 00:32:15 +0530

    updated site

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,26 +1,18 @@
-const darkModeQuery = window.matchMedia('(prefers-color-scheme: dark)');
+const darkModeQuery = window.matchMedia("(prefers-color-scheme: dark)")
 
 function loadTheme() {
-	var cssId = 'myCss';
-
+	var cssId = "myCss"
 	if (!document.getElementById(cssId)) {
-		var head = document.getElementsByTagName('head')[0];
-		var link = document.createElement('link');
-
-		link.id = cssId;
-		link.rel = 'stylesheet';
-		link.type = 'text/css';
-		link.media = 'all';
-
-    		if (darkModeQuery.matches) {
-			link.href = 'themes/dark.css';
-		} else {
-			link.href = 'themes/light.css';
-		}
-
-		head.appendChild(link);
+		var head = document.getElementsByTagName("head")[0]
+		var link = document.createElement("link")
+		link.id = cssId
+		link.rel = "stylesheet"
+		link.type = "text/css"
+		link.media = "all"
+    	link.href = darkModeQuery.matches? "themes/dark.css": "themes/light.css"
+		head.appendChild(link)
 	}
 }
 
-loadTheme();
-darkModeQuery.addEventListener('change', loadTheme);
+loadTheme()
+darkModeQuery.addEventListener("change", loadTheme)
